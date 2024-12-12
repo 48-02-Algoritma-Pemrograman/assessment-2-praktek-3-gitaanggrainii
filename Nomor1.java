d304abe007636d97b4fb53cac00b47d045b2ca0f
gitaanggrainii <gitaanggraini2005@gmail.com>
2024-12-12 14:34:05 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,71 +1,21 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ass22 {
-    public static void main(String[] args){
+public class ass1 {
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Double> totalBiaya = new ArrayList<>();
-
-        while (true) {
-            System.out.print("Masukan pilihan hampers [1/2/3]: ");
-            int hampers = input.nextInt();
-
-            System.out.print("Masukan pilihan packing [1/2/3]: ");
-            int packing = input.nextInt();
-
-            double total = hitungTotalBiaya(hampers, packing);
-            totalBiaya.add(total);
-
-            System.out.print("Input lagi (Y/N)?: ");
-            String jawaban = input.next();
-            if (jawaban.equalsIgnoreCase("N")) {
-                break;
+        int N = input.nextInt();
+        for (int i = 0; i <= N; i++) {
+            if (i % 3 == 1 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
             }
         }
-
-        double totalBayar = hitungTotalBayar(totalBiaya);
-        System.out.println("Total hamper yang dipesan = " + totalBiaya.size());
-        System.out.println("Total yang harus dibayar = Rp " + totalBayar);
-    }
-
-    public static double hitungTotalBiaya(int hampers, int packing) {
-        double hargaHampers = 0;
-        double hargaPacking = 0;
-
-        switch (hampers) {
-            case 1:
-                hargaHampers = 150000;
-                break;
-            case 2:
-                hargaHampers = 175000;
-                break;
-            case 3:
-                hargaHampers = 200000;
-                break;
-        }
-
-        switch (packing) {
-            case 1:
-                hargaPacking = 50000;
-                break;
-            case 2:
-                hargaPacking = 75000;
-                break;
-            case 3:
-                hargaPacking = 125000;
-                break;
-        }
-
-        return hargaHampers + hargaPacking;
-    }
-
-    public static double hitungTotalBayar(ArrayList<Double> totalBiaya) {
-        double totalBayar = 0;
-        for (double biaya : totalBiaya) {
-            totalBayar += biaya;
-        }
-        return totalBayar * 1.1;
+        System.out.println(ass1.class.getDeclaredMethods().length);
     }
 }
-
-
